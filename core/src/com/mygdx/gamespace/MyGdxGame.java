9e4cf9f848ec75b652bf5e8eff32c0897259afa6
vasy <solomon202.pi@yandex.ru>
2021-05-12 22:05:08 +0300

    three

--- core/src/com/mygdx/gamespace/MyGdxGame.java
+++ core/src/com/mygdx/gamespace/MyGdxGame.java
@@ -42,6 +42,8 @@ public class MyGdxGame extends ApplicationAdapter {
 //		запуск метода связаного класса 
       bg.update();
       
+      
+      
 	}
 //	освободить ресурсы 
 	@Override
--- core/src/com/mygdx/gamespace/Shuttle.java
+++ core/src/com/mygdx/gamespace/Shuttle.java
@@ -1,24 +1,29 @@
 package com.mygdx.gamespace;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-
+import com.badlogic.gdx.Input.Keys;
 public class Shuttle {
 	
 	Texture img;
 	Vector2 position;
 	
+	
+
+	float marioX;
+	float marioY;
+	
 //создаем картинку и позицию 
     public Shuttle(){
-        img = new Texture("shuttl.jpg");
-        position = new Vector2(100,380);
+        img = new Texture("shuttl.gif");
+        position = new Vector2(250,20);
         
     }
   //ресуем 
     public void render(SpriteBatch batch){
         batch.draw(img,position.x,position.y);
     }
-	
-
+    
 }
